7d00ab423e027cc6465d1240534d197d7c54e276
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2024-04-21 08:20:16 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,84 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/L2.png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">Creating Connections Between You and Your Dream Home.</p><br>
+										<div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">CustomerCare</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="login.php" class="text-white">Login</a></li>
+                                                    <li><a href="#" class="text-white">Feedback</a></li>
+                                                    <li><a href="Privacy Policy.php" class="text-white">T & C</a></li>
+													<li><a href="contact.php" class="text-white">Center</a></li>
+										
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Company</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">About Us</a></li>
+                                                    <li><a href="#" class="text-white">Feedback</a></li>
+                                                    <li><a href="Privacy Policy.php" class="text-white">Terms and Condition</a></li>
+													<li><a href="contact.php" class="text-white">Location Map</a></li>
+													<li><a href="#" class="text-white">Privacy Policy</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Our Partners</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="https://www.naukri.com/" class="text-white">Naukri.com - Jobs in India</a></li>
+                                                    <li><a href="https://www.onlinesbi.sbi/" class="text-white">StateBankOfIndia.com</a></li>
+                                                    <li><a href="https://www.paisabazaar.com/" class="text-white">PaisaBazaar.com</a></li>
+                                                    <li><a href="https://www.rajhansrealty.co.in/" class="text-white">Ranjhans.com</a></li>
+													<li><a href="https://www.99acres.com/" class="text-white">99acres.com</a></li>
+													<li><a href="https://www.shiksha.com/" class="text-white">Shiksha.com</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+										
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>15A, Vesu, Surat, Gujarat, India.</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+1 234-567-8910</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+91 8735908517</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>apnaghar024@gmail.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="https://www.facebook.com/share/Z3pLN9jLRyX8E5vz/?mibextid=qi2Omg"><i class="fab fa-facebook-f"></i></a> <a href="https://x.com/Dhyeychauhan3?t=RUq6nVwm9omdyzCnx4oDvw&s=09"><i class="fab fa-twitter"></i></a> <a href="https://youtube.com/@dhyeychauhan6793?si=PdN31aD2dPy7YR5S"><i class="fab fa-youtube"></i></a> <a href="https://www.linkedin.com/in/dhyey-chauhan-b3b643255"><i class="fab fa-linkedin-in"></i></a> <a href="https://www.instagram.com/chauhan.dhyey?igsh=dmhrMmU2cGhjaTJs"><i class="fab fa-instagram"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6" align="center"> <span class="text-white">© <?php echo date('Y');?> Apna Ghar Real Estate Website - Developed By SSASITIANS</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="Privacy Policy.php">Privacy & Policy</a></li>
+                            <li>|</li>
+                            <li><a href="#"> Site Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
